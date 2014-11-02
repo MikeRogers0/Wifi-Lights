cb96e45ca8bdd84c5f008060377749c09dafb9fa
Mike Rogers <me@mikerogers.io>
2014-11-02 13:10:27 +0000

    Initial Commit

---
+++ Wifi Lights/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wifi Lights
+//
+//  Created by Mike Rogers on 02/11/2014.
+//  Copyright (c) 2014 Mike Rogers. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Wifi Lights/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Wifi Lights
+//
+//  Created by Mike Rogers on 02/11/2014.
+//  Copyright (c) 2014 Mike Rogers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
