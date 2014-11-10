c5d0669c314cf34f1ad6ae13306fe3df974f865e
Mike Rogers <me@mikerogers.io>
2014-11-10 13:21:13 +0000

    The buttons only bloody do things!

--- Wifi Lights/FirstViewController.swift
+++ Wifi Lights/FirstViewController.swift
@@ -9,19 +9,32 @@
 import UIKit
 
 class FirstViewController: UIViewController{
-    let lightSwitches = LightSwitches(address: "192.168.1.100", port: 5000)
+    let lightSwitches = LightSwitches(address: "192.168.1.100", port: 50000)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        lightSwitches.lightsOn()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func touchLightsOn() {
+        lightSwitches.on();
+    }
+    
+    @IBAction func touchLightsOff() {
+        lightSwitches.off();
+    }
+
+    @IBAction func touchBrightest() {
+        lightSwitches.brightest();
+    }
+    
+    @IBAction func touchDarkest() {
+        lightSwitches.darkest();
     }
 }
 
--- Wifi Lights/LightSwitches.swift
+++ Wifi Lights/LightSwitches.swift
@@ -9,31 +9,53 @@
 import Foundation
 import UIKit
 
-class LightSwitches: GCDAsyncUdpSocketDelegate{
+class LightSwitches: GCDAsyncUdpSocketDelegate {
     let error : NSError?
     var port : UInt16
-    var address : String?
-    var udpSocket : GCDAsyncUdpSocket?
+    var address : String!
+    var udpSocket : GCDAsyncUdpSocket!
+    let commands : [String: NSData] = [
+        "on": NSData(bytes: [0x22, 0x00, 0x55] as [Byte], length: 3),
+        "off": NSData(bytes: [0x21, 0x00, 0x55] as [Byte], length: 3),
+        "brighter": NSData(bytes: [0x23, 0x00, 0x55] as [Byte], length: 3),
+        "darker": NSData(bytes: [0x24, 0x00, 0x55] as [Byte], length: 3)
+    ];
     
     init(address: String?, port: UInt16){
         self.address = address;
         self.port  = port;
+        self.udpSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: dispatch_get_main_queue());
     }
     
-    func lightsOn(){
-        var udpSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: dispatch_get_main_queue());
-      
-        var lightsOnCommand  =  "teststring".dataUsingEncoding(NSUTF8StringEncoding)
-        
-        println("Turning on lights")
-        
-        udpSocket.sendData(lightsOnCommand, toHost: address, port:port, withTimeout: -1, tag: 1)
+    func on(){
+        self.udpSocket.sendData(commands["on"], toHost: address, port:port, withTimeout: -1, tag: 1);
     }
     
-    func udpSocket(sock : GCDAsyncUdpSocket!, didReceiveData data : NSData!,  fromAddress address : NSData!,  withFilterContext filterContext : AnyObject!) {
-        println(data)
+    func off(){
+        self.udpSocket.sendData(commands["off"], toHost: address, port:port, withTimeout: -1, tag: 2);
     }
     
-   
-
+    @objc func brighter(){
+        self.udpSocket.sendData(commands["brighter"], toHost: address, port:port, withTimeout: -1, tag: 3);
+    }
+    
+    @objc func darker(){
+        self.udpSocket.sendData(commands["darker"], toHost: address, port:port, withTimeout: -1, tag: 4);
+    }
+    
+    func brightest(){
+        var delay = 0.0
+        for index in 1...6 {
+            delay += 0.2
+            NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: Selector("brighter"), userInfo: nil, repeats: false)
+        }
+    }
+    
+    func darkest(){
+        var delay = 0.0
+        for index in 1...6 {
+            delay += 0.2
+            NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: Selector("darker"), userInfo: nil, repeats: false)
+        }
+    }
 }
